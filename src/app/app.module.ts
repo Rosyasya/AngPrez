e687a663bf117967610f3ca4787fa96a9d9c7fee
Ростиslave <opamkgue@ukr.net>
2023-07-31 16:33:22 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/ui-kit/header/header.component';
+import { SitesComponent } from './pages/sites/sites.component';
+import {RouterLink, RouterLinkActive, RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import { CardComponent } from './components/card/card.component';
+
+const appRoutes: Routes = [
+  { path: 'sites', component: SitesComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SitesComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
