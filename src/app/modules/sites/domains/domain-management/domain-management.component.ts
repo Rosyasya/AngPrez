79db30986eefd3c9fb61dda3070fc35bbb8ff822
Ростиslave <opamkgue@ukr.net>
2023-08-10 15:33:46 +0300

    review

--- src/app/modules/sites/domains/domain-management/domain-management.component.ts
+++ src/app/modules/sites/domains/domain-management/domain-management.component.ts
@@ -1,20 +1,24 @@
-import {Component} from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-domain-management',
   templateUrl: './domain-management.component.html',
   styleUrls: ['./domain-management.component.scss']
 })
-export class DomainManagementComponent{
-  constructor(private router: Router) {
+export class DomainManagementComponent implements OnInit{
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
   }
   tabs = DomainTabs.values();
   activeTab: DomainTabs;
-
+  id: string = this.activatedRouter.snapshot.url[1].path;
   switchTab(tab: DomainTabs) {
     this.activeTab = tab;
-    this.router.navigate(['domains', '1', tab.name.toLowerCase()]);
+    this.router.navigate(['domains', this.id, tab.name.toLowerCase()]);
+  }
+
+  ngOnInit(): void {
+    console.log(this.id);
   }
 }
 
